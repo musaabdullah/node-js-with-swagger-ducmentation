7a0973228f30455a24444ea3d0aff7a4536dd29b
musaabdullah <musaabdalwahed@gmail.com>
2021-02-10 12:29:43 +0200

    post doc

--- app.js
+++ app.js
@@ -4,27 +4,23 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-const postRouter = require('./routers/posts');
+// const postRouter = require('./routers/posts');
 const port = process.env.PORT | 3000;
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-     info: {
-       title: 'Swagger Express API',
-       version: '1.0.0'
-     },
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Node js documentation API',
+      version: '1.0.0'
+    }
   },
-  apis: ['./routers/posts.js']
+  // apis: ['.routers/*.js'],
+  apis: ['app.js']
 };
 
-const swaggerSpec = swaggerJSDoc(options);
-
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use(cors('dev'));
 app.use('/public',express.static(path.join(__dirname,'public'))); 
 app.use(bodyParser.json());
@@ -34,14 +30,58 @@ app.set('view engine', 'ejs');
 
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-mongoose.connect("mongodb://localhost:27017/hell", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then((res) => { console.log('connected successfuly'); })
-    .catch((err) => { console.log('an error has occard'); });
-app.use(postRouter);
+
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     description: Get all posts 
+ *     responses:
+ *         200:
+ *           description: Success
+ */
+app.get('/posts', async (req, res) => {
+  try {
+    res.status(200).json({ message: 'posts data' })
+  } catch(error) {
+      res.json(error);
+  }
+})
+/**
+ * @swagger 
+ * /posts:
+ *   post: 
+ *     summary: insert new post
+ *     description: insert new post
+ *     requestBody:
+ *      content:
+ *      application/json:
+ *      schema:
+ *        properties:
+ *         title:
+ *          type: string
+ *          description: post title
+ *         body:
+ *          type: string
+ *          description: body of the post
+ *        responses:
+ *         201:
+ *          description: post has been inserted
+ *         422:
+ *          description: post already exists
+ *
+ */
+app.post('/posts', async (req, res) => {
+  try {
+    const post = { title, body } = req.body;
+    res.status(201).json({ post })
+  } catch(error ) {
+    res.status(400).json(error);
+  }
+})
+
+// app.use(postRouter);
 
 app.listen(port, () => {
     console.log(port);
